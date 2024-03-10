43046d6e91c78fedb9a495a61ef524aa3e6742a6
kouqurong <koqurong@outlook.com>
2024-03-10 18:41:23 +0800

    添加缺少依赖

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::default().build())
         .plugin(tauri_plugin_sql::Builder::default().build())
         .run(tauri::generate_context!())
