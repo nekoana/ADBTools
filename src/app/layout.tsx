f6c7f54597e95e8c362627692a83357e842d1db2
kouqurong <koqurong@outlook.com>
2024-12-07 20:48:42 +0800

    update some component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,37 +20,40 @@ function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div data-tauri-drag-region className="titlebar">
-          <div className="relative flex items-center w-auto h-full">
-            <img
+      <div data-tauri-drag-region className="fixed titlebar flex flex-row max-w-full  top-0 left-0 right-0">
+        <div className="relative flex items-center justify-center w-auto h-full mx-auto" id="search-area">
+          <img
               src="https://api.iconify.design/mdi:search.svg"
               alt="search"
               className="absolute left-2 w-5 h-5"
-            />
-            <input
-              className="appearance-none w-36 h-full border-0 border-b-2 rounded-b hover:w-48 focus:w-48 outline-none focus-border-b-primary px-8 bg-color-background transition-all"
+          />
+          <input
+              className="appearance-none w-48 h-full border-0 border-b-2 rounded-b hover:w-64 focus:w-64 outline-none focus-border-b-primary px-8 bg-color-background transition-all"
               onChange={(e) => {
                 setSearchText(e.target.value);
               }}
-            />
-          </div>
+          />
+        </div>
+
+        <div className="ml-auto absolute right-0" id="control-area">
           <div className="titlebar-button" id="titlebar-minimize">
             <img
-              src="https://api.iconify.design/mdi:window-minimize.svg"
-              alt="minimize"
+                src="https://api.iconify.design/mdi:window-minimize.svg"
+                alt="minimize"
             />
           </div>
           <div className="titlebar-button" id="titlebar-close">
-            <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+            <img src="https://api.iconify.design/mdi:close.svg" alt="close"/>
           </div>
         </div>
-        <SearchContext.Provider value={searchText}>
-          <div className="contentbody">{children}</div>
-        </SearchContext.Provider>
+      </div>
+      <SearchContext.Provider value={searchText}>
+        <div className="contentbody">{children}</div>
+      </SearchContext.Provider>
       </body>
     </html>
   );
 }
 
 export default RootLayout;
-export { SearchContext };
+export {SearchContext};
