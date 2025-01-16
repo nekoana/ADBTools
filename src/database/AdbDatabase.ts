756e12efe1178af18c23a47c3a1b96600ca6e096
kouqurong <koqurong@outlook.com>
2025-01-16 21:41:04 +0800

    fix some issue

--- src/database/AdbDatabase.ts
+++ src/database/AdbDatabase.ts
@@ -1,7 +1,5 @@
 import Database from "@tauri-apps/plugin-sql";
 
-const DB = await Database.load("sqlite:adbtools.db");
-
 const config: {
   version: number;
   migrations: Array<{
@@ -13,24 +11,28 @@ const config: {
   migrations: [],
 };
 
-await DB.execute(
-  "CREATE TABLE IF NOT EXISTS cmd (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, command TEXT, keywords TEXT)",
-);
+export async function loadDatabase() {
+  const DB = await Database.load("sqlite:adbtools.db");
 
-//check database version
-let { version } = await DB.select<{ version: number }>("PRAGMA user_version");
-//migration
-const migrations = config.migrations.filter(
-  (migration) => version < migration.version,
-);
-try {
-  for (const migration of migrations) {
-    await migration.up(DB);
-    version = migration.version;
-    await DB.execute("PRAGMA user_version = ?", [version]);
+  await DB.execute(
+    "CREATE TABLE IF NOT EXISTS cmd (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, command TEXT, keywords TEXT)",
+  );
+
+  //check database version
+  let { version } = await DB.select<{ version: number }>("PRAGMA user_version");
+  //migration
+  const migrations = config.migrations.filter(
+    (migration) => version < migration.version,
+  );
+  try {
+    for (const migration of migrations) {
+      await migration.up(DB);
+      version = migration.version;
+      await DB.execute("PRAGMA user_version = ?", [version]);
+    }
+  } catch (error) {
+    console.error("Migration error", error);
   }
-} catch (error) {
-  console.error("Migration error", error);
-}
 
-export { DB };
+  return DB;
+}
